aa8e1e45a5c832af772c32185b80198902774a9a
abigail-Bridget <abigailbridget@outlook.com>
2024-06-23 20:55:44 +0200

    my whole game

--- src/js/game.js
+++ src/js/game.js
@@ -1,27 +1,68 @@
-import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
-import { Resources, ResourceLoader } from './resources.js'
+import '../css/style.css';
+import { Actor, Engine, DisplayMode } from 'excalibur';
+import { Resources, ResourceLoader } from './resources.js';
+import { StartScene } from './startScene.js';
+import { InstructionScene } from './instructionScene.js';
+import { MainGameScene } from './MainGameScene.js';
+import { GameOverScene } from './gameOverScene.js';
+import { EndScene } from './endScene.js';
 
 export class Game extends Engine {
-
+    
     constructor() {
-        super({ 
+        super({
             width: 1280,
             height: 720,
             maxFps: 60,
-            displayMode: DisplayMode.FitScreen
-         })
-        this.start(ResourceLoader).then(() => this.startGame())
+            displayMode: DisplayMode.FitScreen,
+            antialiasing: false,
+        });
+
+        // Load resources before starting the game
+        this.start(ResourceLoader).then(() => this.showStart()).catch(error => {
+            console.error("Error starting the game:", error);
+        });
     }
 
-    startGame() {
-        console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+    showStart() {
+        try {
+            const startScene = new StartScene(this);
+            this.addScene('start', startScene);
+            this.goToScene('start');
+        } catch (error) {
+            console.error("Error during showStart:", error);
+        }
+    }
+
+    goToInstructionScene() {
+        try {
+            const instructionScene = new InstructionScene(this);
+            this.addScene('instructionscene', instructionScene);
+            this.goToScene('instructionscene');
+        } catch (error) {
+            console.error("Error during goToInstructionScene:", error);
+        }
+    }
+
+    goToMainGameScene() {
+        try {
+            const mainScene = new MainGameScene(this);
+            this.addScene('main', mainScene);
+            this.goToScene('main');
+        } catch (error) {
+            console.error("Error during goToMainGameScene:", error);
+        }
+    }
+
+    goToEndScene() {
+        try {
+            const endScene = new EndScene(this);
+            this.add('end', endScene);
+            this.goToScene('end');
+        } catch (error) {
+            console.error("Error during goToEndScene:", error);
+        }
     }
 }
 
-new Game()
+new Game();
